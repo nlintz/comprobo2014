906bf0799b242589f24308ef795749e758b1e7a3
Nathan Lintz <nlintz@gmail.com>
2014-10-28 18:26:48 -0400

    added lib

---
+++ src/computer_vision/lib/BackgroundSubtractor.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+fgbg = cv2.BackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+    blur = cv2.GaussianBlur(fgmask,(5,5),0)
+    ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # hull = cv2.convexHull(fgmask)
+    
+    cv2.imshow('frame',thresh1)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
