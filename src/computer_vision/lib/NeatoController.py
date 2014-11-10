4c95efe1e4cd75fe0059e8588e41579551c2e4a9
Nathan Lintz <nlintz@gmail.com>
2014-11-09 23:14:17 -0500

    event emitter and neato controller

---
+++ src/computer_vision/lib/NeatoController.py
@@ -0,0 +1,25 @@
+from geometry_msgs.msg import Twist, Vector3
+import rospy
+
+class NeatoController():
+	def __init__(self):
+		self.publisher = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=10)
+	
+	def left(self):
+		self.publisher.publish(Twist(linear=Vector3(0.3, 0.0, 0), angular=Vector3(z=.8)))
+
+	def right(self):
+		self.publisher.publish(Twist(linear=Vector3(0.3, 0.0, 0), angular=Vector3(z=-.8)))
+
+	def up(self):
+		self.publisher.publish(Twist(linear=Vector3(0.5, 0.0, 0)))
+
+	def stop(self):
+		self.publisher.publish(Twist())
+
+def main():
+	commands = NeatoCommands()
+	rospy.init_node('rospy_controller', anonymous=True)
+	r = rospy.Rate(10) # 10hz
+	# while not rospy.is_shutdown():
+		
